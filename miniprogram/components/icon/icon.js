62ee823530644d461b05246e8cea1193f778cfc4
lixiong <leox520@163.com>
2026-06-16 00:25:53 +0800

    修复体验版图标显示并优化资源打包

--- miniprogram/components/icon/icon.js
+++ miniprogram/components/icon/icon.js
@@ -1,8 +1,12 @@
+const ICONFONT_MAP = require('./iconfont-map')
+const FALLBACK_ICON = 'circle-help'
+
 Component({
   properties: {
     name: {
       type: String,
-      value: 'circle-help'
+      value: FALLBACK_ICON,
+      observer: 'updateGlyph'
     },
     color: {
       type: String,
@@ -11,6 +15,27 @@ Component({
     size: {
       type: Number,
       value: 32
+    }
+  },
+
+  data: {
+    glyph: ''
+  },
+
+  lifetimes: {
+    attached() {
+      this.updateGlyph(this.data.name)
+    }
+  },
+
+  methods: {
+    updateGlyph(name) {
+      const codePoint = ICONFONT_MAP[name] || ICONFONT_MAP[FALLBACK_ICON]
+      const glyph = String.fromCharCode(codePoint)
+
+      if (glyph !== this.data.glyph) {
+        this.setData({ glyph })
+      }
     }
   }
 })
